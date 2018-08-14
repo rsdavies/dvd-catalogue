4a676b8852c19a25274f129f55c8406d373db5ba
rhiannonsteele <r.s.steele8@gmail.com>
2018-08-14 21:57:19 +0100

    added methods for name, where and date

--- dvds/models.py
+++ dvds/models.py
@@ -7,3 +7,12 @@ class dvd(models.Model):
     name = models.CharField(max_length=200)
     release_date = models.DateTimeField('date released')
     where_stored = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def where(self):
+        return self.where_stored
+
+    def released(self):
+        return self.release_date
