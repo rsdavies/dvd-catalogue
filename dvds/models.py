d87eebba104818a25579de46e9794e7f2622a3bc
rhiannonsteele <r.s.steele8@gmail.com>
2018-08-15 20:10:59 +0100

    improved the models to include more tables

--- dvds/models.py
+++ dvds/models.py
@@ -3,10 +3,41 @@ from django.db import models
 # Create your models here.
 
 
-class dvd(models.Model):
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location_name
+
+
+class DvD(models.Model):
     name = models.CharField(max_length=200)
     release_date = models.DateTimeField('date released')
-    where_stored = models.CharField(max_length=30)
+    where_stored = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=30)
+    film = models.ForeignKey(DvD, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=30)
+    film = models.ForeignKey(DvD, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=30)
+    film = models.ForeignKey(DvD, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
