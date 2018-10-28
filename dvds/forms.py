3daae0fb4c0c2ee5d171ffab712fdec09c454b9e
rhiannonsteele <r.s.steele8@gmail.com>
2018-10-28 17:18:32 +0000

    fixing date input

--- dvds/forms.py
+++ dvds/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class DvDForm(forms.Form):
     film_name = forms.CharField(label="what the dvd's name?", max_length=200, required=True)
-    film_year = forms.DateField(label="when was it released?", input_formats=['%Y-%m-%d'], required=False)
+    film_year = forms.CharField(label="what year was it released?", required=False)
     film_location = forms.CharField(label="where does it live?", max_length=30, required=True)
-    is_movie = forms.ChoiceField(choices=[(True, 'film'), (False, 'series')])
+    is_movie = forms.ChoiceField(label="Its is a ", choices=[(True, 'film'), (False, 'series')])
